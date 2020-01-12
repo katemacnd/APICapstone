dc427a86e2559ef028f86f2d28049ffb2b881aac
Kate Engstrom <katemacnd@gmail.com>
2020-01-12 12:19:40 -0600

    add proxy switch to XML

--- main.js
+++ main.js
@@ -4,21 +4,6 @@
 $(document).ready(function () {
   'use strict';
 
-//
-// // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-// if (!event.target.matches('.dropbtn')) {
-//   var dropdowns = document.getElementsByClassName("dropdown-content");
-//   var i;
-//   for (i = 0; i < dropdowns.length; i++) {
-//     var openDropdown = dropdowns[i];
-//     if (openDropdown.classList.contains('show')) {
-//       openDropdown.classList.remove('show');
-//     }
-//   }
-// }
-// };
-
   function watchForm() {
     $('#searchInput').click(event => {
       event.preventDefault();
@@ -26,20 +11,26 @@ $(document).ready(function () {
         console.log(searchTerms);
       $('#searchBar').toggle('hidden');
       let appID = 'Katherin-APICapst-PRD-c82b90351-fbc84e5e';
-      let url = `https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${appID}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=${searchTerms}`;
+      let url = `https://cors-anywhere.herokuapp.com/http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${appID}&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=${searchTerms}`;
         console.log(url);
-      fetch(url).then((response) => {
-        let data = response.json();
-        data.forEach((subData => {
-          let pargraph = document.createElement('p');
-          paragraph.innerText = subData.itemName;
-      //     let image =
-      //     let price =)
-
-      }));
-    });
-  });
-}
+      let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState !== 4) return;
+          if (xhr.status === 200) {
+            let data = xhr.responseText;
+            console.log(data);
+            var header = data;
+            let paragraph = document.createElement('p');
+            paragraph.innerText = header;
+          }
+          else {
+            console.log('HTTP error', xhr.status, xhr.statusText);
+          }
+        };
+      xhr.send();
+      });
+    }
 
   function watchDrop() {
     $('.dropdownOptions').click(event => {
@@ -53,19 +44,18 @@ $(document).ready(function () {
       $(watchForm);
       });
     }
-
- $(watchDrop);
-
+   $(watchDrop);
 });
 
 
-// pseudocode untouched
+// pseudocode untouched --
   // unhide headline3
-  // Fetch
+  // Display
     // Item Picture
     // Item name
     // Stars/Ratings
     // Top Comment
       // expand description functionality
     // link to the ebay page to purchase
-  // new search button
+  // "new search" button
+  // Close the dropdown menu if the user clicks outside of it
