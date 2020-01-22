614c5f0621c2edf6d3603a9eb46be80f6af5b40e
Kate Engstrom <katemacnd@gmail.com>
2020-01-22 12:00:45 -0600

    scrolling dropdown, links open new tab, dropdown affects search results

--- main.js
+++ main.js
@@ -15,66 +15,6 @@ $(document).ready(function() {
     });
   }
 
-  function watchForm() {
-    $('#searchInput').click(event => {
-      event.preventDefault();
-      const searchTerms = ($('#searchTerms').val());
-      console.log(searchTerms);
-      $('#searchBar').toggle('hidden');
-      let appID = 'Katherin-APICapst-PRD-c82b90351-fbc84e5e';
-      let url = `https://cors-anywhere.herokuapp.com/http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${appID}&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=${searchTerms}`;
-      console.log(url);
-      let xhr = new XMLHttpRequest();
-      xhr.open('GET', url);
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState !== 4) return;
-        if (xhr.status === 200) {
-
-          var parseXml;
-
-          if (typeof window.DOMParser != "undefined") {
-            parseXml = function(xmlStr) {
-              return (new window.DOMParser()).parseFromString(xmlStr, "text/xml");
-            };
-          } else if (typeof window.ActiveXObject != "undefined" &&
-            new window.ActiveXObject("Microsoft.XMLDOM")) {
-            parseXml = function(xmlStr) {
-              var xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
-              xmlDoc.async = "false";
-              xmlDoc.loadXML(xmlStr);
-              return xmlDoc;
-            };
-          } else {
-            throw new Error("No XML parser found");
-          }
-
-          let data = xhr.responseText;
-          console.log(data);
-          var xmlDoc = parseXml(data);
-          $('#finalScreen').toggle('hidden');
-          for (let i = 0; i < 5; i++) {
-            var x = document.createElement("P");
-            // var picture = document.createTextNode(xmlDoc.getElementsByTagName("galleryURL")[i].childNodes[0].nodeValue);
-            var picture = document.createElement('img');
-            picture.src = xmlDoc.getElementsByTagName("galleryURL")[i].childNodes[0].nodeValue;
-            x.appendChild(picture);
-            var anchor = document.createElement('a');
-            var title = document.createTextNode(xmlDoc.getElementsByTagName("title")[i].childNodes[0].nodeValue );
-            anchor.appendChild(title);
-            anchor.href = xmlDoc.getElementsByTagName("viewItemURL")[i].childNodes[0].nodeValue;
-            //var title = document.createTextNode(xmlDoc.getElementsByTagName("title")[i].childNodes[0].nodeValue);
-            x.appendChild(anchor);
-            var price = document.createTextNode('$' + xmlDoc.getElementsByTagName("convertedCurrentPrice")[i].childNodes[0].nodeValue);
-            x.appendChild(price);
-            document.getElementById("results").appendChild(x);
-          }
-        }
-      };
-      xhr.send();
-      $(newSearch);
-    });
-  }
-
   function watchDrop() {
     $('.dropdownOptions').click(event => {
       event.preventDefault();
@@ -84,8 +24,68 @@ $(document).ready(function() {
       $('#neededAnimal').hide();
       $('#selectedAnimal').append(animalClicked + "?");
       $('#searchBar').toggle('hidden');
-      $(watchForm);
+      watchForm(animalClicked);
     });
+
+    function watchForm(animalClicked) {
+      $('#searchInput').click(event => {
+        event.preventDefault();
+        const searchTerms = ($('#searchTerms').val());
+        var fullSearch = animalClicked + " " + searchTerms;
+        console.log(searchTerms);
+        $('#searchBar').toggle('hidden');
+        let appID = 'Katherin-APICapst-PRD-c82b90351-fbc84e5e';
+        let url = `https://cors-anywhere.herokuapp.com/http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${appID}&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=${fullSearch}`;
+        console.log(url);
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState !== 4) return;
+          if (xhr.status === 200) {
+
+            var parseXml;
+
+            if (typeof window.DOMParser != "undefined") {
+              parseXml = function(xmlStr) {
+                return (new window.DOMParser()).parseFromString(xmlStr, "text/xml");
+              };
+            } else if (typeof window.ActiveXObject != "undefined" &&
+              new window.ActiveXObject("Microsoft.XMLDOM")) {
+              parseXml = function(xmlStr) {
+                var xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
+                xmlDoc.async = "false";
+                xmlDoc.loadXML(xmlStr);
+                return xmlDoc;
+              };
+            } else {
+              throw new Error("No XML parser found");
+            }
+
+            let data = xhr.responseText;
+            console.log(data);
+            var xmlDoc = parseXml(data);
+            $('#finalScreen').toggle('hidden');
+            for (let i = 0; i < 5; i++) {
+              var x = document.createElement("P");
+              var picture = document.createElement('img');
+              picture.src = xmlDoc.getElementsByTagName("galleryURL")[i].childNodes[0].nodeValue;
+              x.appendChild(picture);
+              var anchor = document.createElement('a');
+              var title = document.createTextNode(xmlDoc.getElementsByTagName("title")[i].childNodes[0].nodeValue );
+              anchor.appendChild(title);
+              anchor.href = xmlDoc.getElementsByTagName("viewItemURL")[i].childNodes[0].nodeValue;
+              var externalLink = x.appendChild(anchor);
+              $(externalLink).attr("target","_blank");
+              var price = document.createTextNode('$' + xmlDoc.getElementsByTagName("convertedCurrentPrice")[i].childNodes[0].nodeValue);
+              x.appendChild(price);
+              document.getElementById("results").appendChild(x);
+            }
+          }
+        };
+        xhr.send();
+        $(newSearch);
+      });
+    }
   }
 
   function watchDrop2() {
@@ -183,22 +183,19 @@ $(document).ready(function() {
 
   $(watchSearch);
 
-
-  /// clicking eBay lower nav button
-
 });
 
 /////// to-dos:
 // enter key initiates search
-// language = english
 // remove duplicate listings
-// add linebreaks to results information
+  //create new array called ‘processedItems’ for example outside the for loop and in each iteration push the viewItemUrls into ‘processedItems’.  
+  //In subsequent iteration, check if the current viewItemUrls is in ‘processedItems’.
+  //If yes, that means the current item is a duplicate of an item we already processed so you skip it by calling ‘return’;
+  //if no, you process it as usual and create the <p>, <img>, and price tags
 // Display
-  // Item Picture
   // Stars/Ratings
   // Top Comment
   // expand description functionality
   // link to the ebay page to purchase
 // Close the dropdown menu if the user clicks outside of it
-// add animal type to search
 // while on goodreads page, make nav at bottom take back to the ebay API search page
