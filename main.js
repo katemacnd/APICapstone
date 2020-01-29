95f65721b81c628a7ce0b752c84edcae7bd2391e
Kate Engstrom <katemacnd@gmail.com>
2020-01-29 13:41:05 -0600

    css and js refactor

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 // https://developer.ebay.com/Devzone/finding/Concepts/FindingAPIGuide.html#usekeywords
 /*jshint esversion: 6 */
-
 $(document).ready(function() {
   'use strict';
-
     /// ebay API
     function newSearch() {
       $('#newSearch').click(event => {
@@ -14,7 +12,6 @@ $(document).ready(function() {
         document.getElementById("results").innerHTML = "";
       });
     }
-
     function watchDrop() {
       $('.dropdownOptions').click(event => {
         event.preventDefault();
@@ -26,7 +23,6 @@ $(document).ready(function() {
         $('#searchBar').show();
         watchForm(animalClicked);
       });
-
       function watchForm(animalClicked) {
         $('#searchInput').click(event => {
           event.preventDefault();
@@ -36,111 +32,39 @@ $(document).ready(function() {
           $('#searchBar').toggle('hidden');
           let appID = 'Katherin-APICapst-PRD-c82b90351-fbc84e5e';
           let url = `https://cors-anywhere.herokuapp.com/http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${appID}&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=${fullSearch}`;
-          console.log(url);
-          let xhr = new XMLHttpRequest();
-          xhr.open('GET', url);
-          xhr.onreadystatechange = function() {
-            if (xhr.readyState !== 4) return;
-            if (xhr.status === 200) {
-
-              var parseXml;
-
-              if (typeof window.DOMParser != "undefined") {
-                parseXml = function(xmlStr) {
-                  return (new window.DOMParser()).parseFromString(xmlStr, "text/xml");
-                };
-              } else if (typeof window.ActiveXObject != "undefined" &&
-                new window.ActiveXObject("Microsoft.XMLDOM")) {
-                parseXml = function(xmlStr) {
-                  var xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
-                  xmlDoc.async = "false";
-                  xmlDoc.loadXML(xmlStr);
-                  return xmlDoc;
-                };
-              } else {
-                throw new Error("No XML parser found");
-              }
-
-              let data = xhr.responseText;
-              console.log(data);
-              var xmlDoc = parseXml(data);
-              $('#finalScreen').toggle('hidden');
-              $('.fa fa-search').hide();
-              for (let i = 0; i < 9; i++) {
-                var x = document.createElement("P");
-                var picture = document.createElement('img');
-                picture.src = xmlDoc.getElementsByTagName("galleryURL")[i].childNodes[0].nodeValue;
-                x.appendChild(picture);
-                var anchor = document.createElement('a');
-                var title = document.createTextNode(xmlDoc.getElementsByTagName("title")[i].childNodes[0].nodeValue);
-                anchor.appendChild(title);
-                anchor.href = xmlDoc.getElementsByTagName("viewItemURL")[i].childNodes[0].nodeValue;
-                var externalLink = x.appendChild(anchor);
-                $(externalLink).attr("target", "_blank");
-                var price = document.createTextNode('$' + xmlDoc.getElementsByTagName("convertedCurrentPrice")[i].childNodes[0].nodeValue);
-                x.appendChild(price);
-                document.getElementById("results").appendChild(x);
-              }
-            }
-          };
-          xhr.send();
+          getEbayItemInfo(url, handleData);
           $(newSearch);
         });
       }
     }
-
-    function watchDrop2() {
-      $('#suppliesUp').click(event => {
-        event.preventDefault();
-        $('.bookSearch').hide();
-        $('#suppliesLowerBar').hide();
-        $('#bookSearchTerms').show();
-        $('#bookSearchTermsButton').show();
-        $('#readUp').show();
-        $('#lowerBar').show();
-        $('#mainContent').show();
-        $('#suppliesUp').hide();
-        $('#suppliesLowerBar').hide();
-        $(watchDrop);
-        $(watchSearch);
-      });
+    function handleData() {
+        var xmlDoc = this;
+        $('#finalScreen').toggle('hidden');
+        $('.fa fa-search').hide();
+        for (let i = 0; i < 9; i++) {
+          var x = document.createElement("P");
+          var picture = document.createElement('img');
+          picture.src = xmlDoc.getElementsByTagName("galleryURL")[i].childNodes[0].nodeValue;
+          x.appendChild(picture);
+          var anchor = document.createElement('a');
+          var title = document.createTextNode(xmlDoc.getElementsByTagName("title")[i].childNodes[0].nodeValue);
+          anchor.appendChild(title);
+          anchor.href = xmlDoc.getElementsByTagName("viewItemURL")[i].childNodes[0].nodeValue;
+          var externalLink = x.appendChild(anchor);
+          $(externalLink).attr("target", "_blank");
+          var price = document.createTextNode('$' + xmlDoc.getElementsByTagName("convertedCurrentPrice")[i].childNodes[0].nodeValue);
+          x.appendChild(price);
+          document.getElementById("results").appendChild(x);
+        }
     }
-
-    $(watchDrop);
-
-    // end eBAY API
-
-
-    // start Goodreads API
-
-    function newBookSearch() {
-      $('#newBookSearch').click(event => {
-        event.preventDefault();
-        console.log('new search!');
-        document.getElementById("bookResults").innerHTML = "";
-        $('#newBookSearch').toggle('hidden');
-        $('#bookSearchTerms').toggle('hidden');
-      });
-    }
-
-    function watchBook() {
-      $('#bookSearchTermsButton').click(event => {
-        event.preventDefault();
-        const searchTerms = ($('#bookSearchTerms').val());
-        console.log(searchTerms);
-        $('#bookSearchTerms').toggle('hidden');
-        $('#newBookSearch').toggle('hidden');
-        let appID = 'CiGujFcIajhkPUPGHkeNg';
-        let url = `https://cors-anywhere.herokuapp.com/http://www.goodreads.com/book/title.xml?title=${searchTerms}&key=${appID}`;
+    function getEbayItemInfo(url, callback) {
         console.log(url);
         let xhr = new XMLHttpRequest();
         xhr.open('GET', url);
         xhr.onreadystatechange = function() {
           if (xhr.readyState !== 4) return;
           if (xhr.status === 200) {
-
             var parseXml;
-
             if (typeof window.DOMParser != "undefined") {
               parseXml = function(xmlStr) {
                 return (new window.DOMParser()).parseFromString(xmlStr, "text/xml");
@@ -157,6 +81,72 @@ $(document).ready(function() {
               throw new Error("No XML parser found");
             }
 
+            let data = xhr.responseText;
+            var xmlDoc = parseXml(data);
+            callback.apply(xmlDoc);
+            console.log(data);
+        }
+    };
+    xhr.send();
+    }
+    function watchDrop2() {
+      $('#suppliesUp').click(event => {
+        event.preventDefault();
+        $('.bookSearch').hide();
+        $('#suppliesLowerBar').hide();
+        $('#bookSearchTerms').show();
+        $('#bookSearchTermsButton').show();
+        $('#readUp').show();
+        $('#upperBar').show();
+        $('#mainContent').show();
+        $('#suppliesUp').hide();
+        $('#suppliesLowerBar').hide();
+        $(watchDrop);
+        $(watchSearch);
+      });
+    }
+    // end eBAY API
+    // start Goodreads API
+    function newBookSearch() {
+      $('#newBookSearch').click(event => {
+        event.preventDefault();
+        console.log('new search!');
+        document.getElementById("bookResults").innerHTML = "";
+        $('#newBookSearch').toggle('hidden');
+        $('#bookSearchTerms').toggle('hidden');
+      });
+    }
+    function watchBook() {
+      $('#bookSearchTermsButton').click(event => {
+        event.preventDefault();
+        const searchTerms = ($('#bookSearchTerms').val());
+        console.log(searchTerms);
+        $('#bookSearchTerms').toggle('hidden');
+        $('#newBookSearch').toggle('hidden');
+        let appID = 'CiGujFcIajhkPUPGHkeNg';
+        let url = `https://cors-anywhere.herokuapp.com/http://www.goodreads.com/book/title.xml?title=${searchTerms}&key=${appID}`;
+        console.log(url);
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState !== 4) return;
+          if (xhr.status === 200) {
+            var parseXml;
+            if (typeof window.DOMParser != "undefined") {
+              parseXml = function(xmlStr) {
+                return (new window.DOMParser()).parseFromString(xmlStr, "text/xml");
+              };
+            } else if (typeof window.ActiveXObject != "undefined" &&
+              new window.ActiveXObject("Microsoft.XMLDOM")) {
+              parseXml = function(xmlStr) {
+                var xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
+                xmlDoc.async = "false";
+                xmlDoc.loadXML(xmlStr);
+                return xmlDoc;
+              };
+            } else {
+              throw new Error("No XML parser found");
+            }
             let data = xhr.responseText;
             console.log(data);
             var xmlDoc = parseXml(data);
@@ -175,12 +165,11 @@ $(document).ready(function() {
         $(newBookSearch);
       });
     }
-
     function watchSearch() {
       $('#readUp').click(event => {
         event.preventDefault();
         $('#mainContent').hide();
-        $('#lowerBar').hide();
+        $('#upperBar').hide();
         $('.bookSearch').show();
         $('.eBay').show();
         $('#suppliesLowerBar').show();
@@ -193,22 +182,18 @@ $(document).ready(function() {
         $(watchDrop2);
       });
     }
-
-    $(watchSearch);
-
     function watchFirst() {
       $('#supplies').click(event => {
         event.preventDefault();
         $('#landingPage').hide();
         $('#secondaryPages').show();
       });
-
       $('#books').click(event => {
         event.preventDefault();
         $('#landingPage').hide();
         $('#secondaryPages').show();
         $('#mainContent').hide();
-        $('#lowerBar').hide();
+        $('#upperBar').hide();
         $('.bookSearch').show();
         $('.eBay').show();
         $('#suppliesLowerBar').show();
@@ -221,11 +206,14 @@ $(document).ready(function() {
         $(watchDrop2);
       });
     }
-
-  $(watchFirst);
+    $(watchDrop);
+    $(watchSearch);
+    $(watchFirst);
 });
 
 /////// to-dos:
+// refactor watchBook function
+// single purpose functions with clean names
 // enter key initiates search
 // remove duplicate listings
     // create new array called ‘processedItems’ for example outside the for loop and in each iteration push the viewItemUrls into ‘processedItems’.
@@ -233,7 +221,7 @@ $(document).ready(function() {
     // If yes, that means the current item is a duplicate of an item we already processed so you skip it by calling ‘return’;
     // if no, you process it as usual and create the <p>, <img>, and price tags
 // Display
-// Stars/Ratings
-// Top Comment
-// expand description functionality
+  // Stars/Ratings
+  // Top Comment
+  // expand description functionality
 // Close the dropdown menu if the user clicks outside of it
